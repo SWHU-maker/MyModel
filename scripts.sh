82c9fb92c3db474119dca1fc251124d4447b5a7d
SWHU-maker <18770815258@163.com>
2026-02-27 22:09:26 +0800

    Router的分Patch编码和标准mamba，多参数组合（bs和drop）尝试

--- scripts.sh
+++ scripts.sh
@@ -7,12 +7,31 @@ model_id_name=weather
 data_name=custom
 random_seed=2024
 
+# for pred_len in 96 192 336 720
+# do
+#     python -u run.py \
+#       --random_seed $random_seed \
+#       --is_training 1 \
+#       --batch_size 32 \
+#       --dropout 0.1 \
+#       --root_path $root_path_name \
+#       --data_path $data_path_name \
+#       --model_id $model_id_name'_'$seq_len'_'$pred_len \
+#       --model $model_name \
+#       --data $data_name \
+#       --features M \
+#       --seq_len $seq_len \
+#       --pred_len $pred_len \
+#       --enc_in 21
+# done
+
+
 for pred_len in 96 192 336 720
 do
     python -u run.py \
       --random_seed $random_seed \
       --is_training 1 \
-      --batch_size 32 \
+      --batch_size 64 \
       --dropout 0.1 \
       --root_path $root_path_name \
       --data_path $data_path_name \
@@ -26,3 +45,39 @@ do
 done
 
 
+for pred_len in 96 192 336 720
+do
+    python -u run.py \
+      --random_seed $random_seed \
+      --is_training 1 \
+      --batch_size 32 \
+      --dropout 0.2 \
+      --root_path $root_path_name \
+      --data_path $data_path_name \
+      --model_id $model_id_name'_'$seq_len'_'$pred_len \
+      --model $model_name \
+      --data $data_name \
+      --features M \
+      --seq_len $seq_len \
+      --pred_len $pred_len \
+      --enc_in 21
+done
+
+
+for pred_len in 96 192 336 720
+do
+    python -u run.py \
+      --random_seed $random_seed \
+      --is_training 1 \
+      --batch_size 64 \
+      --dropout 0.2 \
+      --root_path $root_path_name \
+      --data_path $data_path_name \
+      --model_id $model_id_name'_'$seq_len'_'$pred_len \
+      --model $model_name \
+      --data $data_name \
+      --features M \
+      --seq_len $seq_len \
+      --pred_len $pred_len \
+      --enc_in 21
+done
