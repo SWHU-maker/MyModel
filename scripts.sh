8d960ca138d444c4842a7ef6565b298699291cb1
SWHU-maker <18770815258@163.com>
2026-03-10 20:03:40 +0800

    最佳准度，平均精度是0.242，sota是0.226，增加了日志里的负载均衡超参数指标，设置为1，drop层还没有应用上去

--- scripts.sh
+++ scripts.sh
@@ -11,33 +11,38 @@ random_seed=2024
 pred_lens="96 192 336 720"
 batch_sizes="64 "
 dropouts="0 "
+moe_weights="1"
 
 for bs in $batch_sizes
 do
   for drop in $dropouts
   do
-    for pred_len in $pred_lens
+    for moe_w in $moe_weights
     do
-      echo "Running with pred_len=$pred_len, batch_size=$bs, dropout=$drop"
-      
-      python -u run.py \
-        --random_seed $random_seed \
-        --is_training 1 \
-        --batch_size $bs \
-        --dropout $drop \
-        --root_path $root_path_name \
-        --data_path $data_path_name \
-        --model_id $model_id_name'_'$seq_len'_'$pred_len'_bs'$bs'_drop'$drop \
-        --model $model_name \
-        --data $data_name \
-        --features M \
-        --seq_len $seq_len \
-        --pred_len $pred_len \
-        --enc_in 21 \
-        --gpu 0 \
-      #   --use_multi_gpu \
-      #   --devices '0,1,2,3' \
-      
+      for pred_len in $pred_lens
+      do
+        echo "Running with pred_len=$pred_len, batch_size=$bs, dropout=$drop, moe_loss_weight=$moe_w"
+        
+        python -u run.py \
+          --random_seed $random_seed \
+          --is_training 1 \
+          --batch_size $bs \
+          --dropout $drop \
+          --moe_loss_weight $moe_w \
+          --root_path $root_path_name \
+          --data_path $data_path_name \
+          --model_id $model_id_name'_'$seq_len'_'$pred_len'_bs'$bs'_drop'$drop'_moe'$moe_w \
+          --model $model_name \
+          --data $data_name \
+          --features M \
+          --seq_len $seq_len \
+          --pred_len $pred_len \
+          --enc_in 21 \
+          --gpu 0 \
+        #   --use_multi_gpu \
+        #   --devices '0,1,2,3' \
+        
+      done
     done
   done
 done
